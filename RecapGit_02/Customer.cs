09c194a709458eb34c7dee418a76e78124da3be5
vasileios-k <vkyrikos@gmail.com>
2020-12-30 14:58:46 +0200

    Created the Customer class and an instance of it.

---
+++ RecapGit_02/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecapGit_02
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
